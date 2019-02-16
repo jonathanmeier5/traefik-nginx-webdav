5ba69be2688527f58c2f72ec4f0b4fe3f17fec3d
Jonathan <jonathanwmeier@gmail.com>
2019-02-16 19:16:48 +0000

    Use htpasswd to generate nginx htpasswd.

--- files/usr/local/bin/nginx-entrypoint.sh
+++ files/usr/local/bin/nginx-entrypoint.sh
@@ -7,7 +7,6 @@ NGINX_CONF=/etc/nginx/conf.d/app.conf
 # poor man's interpolation engine
 sed -i -e 's|${CERT_PATH}|'"$CERT_PATH"'|' -e 's|${WEBDAV_DOMAIN}|'"$WEBDAV_DOMAIN"'|' $NGINX_CONF
 
-# inject user data
-echo $WEBDAV_USER_DATA > /etc/nginx/htpasswd
+htpasswd -bc /etc/nginx/htpasswd $WEBDAV_USERNAME $WEBDAV_PASSWORD
 
 exec nginx -g 'daemon off;'
